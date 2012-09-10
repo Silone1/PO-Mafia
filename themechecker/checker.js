261ed814c7d9e0034975e76d8e064d5c7ece1ed1
TheUnknownOne <TheUnknownOne25@Gmail.com>
2012-09-10 22:08:13 +0200

    villageCantLoseRoles fix

--- themechecker/checker.js
+++ themechecker/checker.js
@@ -935,7 +935,11 @@
                         addFatalError("Your \"villageCantLoseRoles\" list contains an invalid role \"" + cantLose[x] + "\".");
                     }
                 }
-            }
+            } else {
+			if (themes.sides.has("village")) {
+			addFatalError("Your theme needs (an empty) \"villageCantLoseRoles\", because you have a side 'village'.");
+			}
+			}
 
             theme.addActions();
             theme.checkActions();
