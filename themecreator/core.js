ca609e5b22b177260787d2efb16671a82a3ba1d6
TheUnknownOne <TheUnknownOne25@Gmail.com>
2012-09-22 18:28:15 +0200

    Themecreator

---
+++ themecreator/core.js
@@ -0,0 +1,225 @@
+/* Utilities */
+(function () {
+    Object.defineProperty(String.prototype, "isEmpty", {
+        "value": function () {
+            var mess = this;
+            return mess == "" || mess.trim() == "";
+        },
+
+        writable: true,
+        enumerable: false,
+        configurable: true
+    });
+
+    Object.defineProperty(String.prototype, "contains", {
+        "value": function (string) {
+            var str = this;
+            return str.indexOf(string) > -1;
+        },
+
+        writable: true,
+        enumerable: false,
+        configurable: true
+    });
+
+    Object.defineProperty(String.prototype, "has", {
+        "value": function (string) {
+            return this.contains(string);
+        },
+
+        writable: true,
+        enumerable: false,
+        configurable: true
+    });
+
+    Object.defineProperty(String.prototype, "format", {
+        "value": function () {
+            var str = this,
+                exp, i, args = arguments.length,
+                icontainer = 0;
+            for (i = 0; i < args; i++) {
+                icontainer++;
+                exp = new RegExp("%" + icontainer, "");
+                str = str.replace(exp, arguments[i]);
+            }
+            return str;
+        },
+
+        writable: true,
+        enumerable: false,
+        configurable: true
+    });
+
+    Object.defineProperty(Boolean.prototype, "isEmpty", {
+        "value": function () {
+            return this === false;
+        },
+
+        writable: true,
+        enumerable: false,
+        configurable: true
+    });
+
+    Object.defineProperty(Number.prototype, "isEmpty", {
+        "value": function () {
+            return isNaN(this) || this === 0;
+        },
+
+        writable: true,
+        enumerable: false,
+        configurable: true
+    });
+
+    Object.defineProperty(Object.prototype, "isEmpty", {
+        "value": function () {
+            return this.length() === 0;
+        },
+
+        writable: true,
+        enumerable: false,
+        configurable: true
+    });
+
+    Object.defineProperty(Object.prototype, "keys", {
+        "value": function () {
+            return Object.keys(this);
+        },
+
+        writable: true,
+        enumerable: false,
+        configurable: true
+    });
+
+    Object.defineProperty(Object.prototype, "has", {
+        "value": function (prop) {
+            return typeof this[prop] !== "undefined";
+        },
+
+        writable: true,
+        enumerable: false,
+        configurable: true
+    });
+
+    Object.defineProperty(Object.prototype, "contains", {
+        "value": function (prop) {
+            return this.has(prop);
+        },
+
+        writable: true,
+        enumerable: false,
+        configurable: true
+    });
+
+    Object.defineProperty(Object.prototype, "insert", {
+        "value": function (name, val) {
+            this[name] = val;
+        },
+
+        writable: true,
+        enumerable: false,
+        configurable: true
+    });
+
+    Object.defineProperty(Object.prototype, "remove", {
+        "value": function (name) {
+            if (!this.has(name)) {
+                return;
+            }
+
+            delete this[name];
+        },
+
+        writable: true,
+        enumerable: false,
+        configurable: true
+    });
+
+    Object.defineProperty(Object.prototype, "first", {
+        "value": function () {
+            var x;
+            for (x in this) {
+                return this[x]; // Grab the first property
+            }
+        },
+
+        writable: true,
+        enumerable: false,
+        configurable: true
+    });
+
+    Object.defineProperty(Object.prototype, "length", {
+        "value": function () {
+            return Object.keys(this).length;
+        },
+
+        writable: true,
+        enumerable: false,
+        configurable: true
+    });
+
+    Object.defineProperty(Array.prototype, "has", {
+        "value": function (prop) {
+            var x;
+            for (x in this) {
+                if (this[x] == prop) {
+                    return true;
+                }
+            }
+
+            return false;
+        },
+
+        writable: true,
+        enumerable: false,
+        configurable: true
+    });
+
+    Object.defineProperty(Array.prototype, "isEmpty", {
+        "value": function () {
+            return this.length === 0;
+        },
+
+        writable: true,
+        enumerable: false,
+        configurable: true
+    });
+
+    Object.defineProperty(Array.prototype, "contains", {
+        "value": function (prop) {
+            return this.has(prop);
+        },
+
+        writable: true,
+        enumerable: false,
+        configurable: true
+    });
+
+    useTheme = function (text) {
+        var textarea = $("#LoadTheme"),
+            button = $("#UseTheme"),
+            val = textarea.val();
+        if (text != undefined) {
+            val = text;
+        }
+
+        if (!loadTheme(val)) {
+            return; // Parsing Error.
+        }
+
+        textarea.fadeOut("slow");
+        button.fadeOut("slow");
+    }
+
+})();
+
+$(document).ready(function () {
+    var CreateNew = $("#CreateNew");
+
+    CreateNew.button();
+    CreateNew.click(function () {
+        useTheme("{}");
+    });
+
+    $("#UseTheme").button();
+    $("#UseExisting").button();
+});
