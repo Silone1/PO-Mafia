2e04f56e52c4f2a4fb1fe64aaca8eb634007d144
TheUnknownOne <TheUnknownOne25@Gmail.com>
2012-09-22 21:59:31 +0200

    Attempt 4

--- themecreator/core.js
+++ themecreator/core.js
@@ -183,7 +183,7 @@ dialog = function (title, text) {
 
     $("#Dialog").html(res).prop("title", title);
 
-    $("#Dialog").dialog({
+    $("#Dialog").dialog("destroy").dialog({
         modal: true,
         width: 300,
         height: 200,
